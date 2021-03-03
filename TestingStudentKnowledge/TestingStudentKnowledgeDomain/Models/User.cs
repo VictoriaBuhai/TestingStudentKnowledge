b1f4efabb662d1a7bf0b122915b2669ea4e54478
MEVIO01 <erik.mirai@outlook.com>
2021-03-03 08:46:32 +0200

    base template for models and migrations added

---
+++ TestingStudentKnowledge/TestingStudentKnowledgeDomain/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingStudentKnowledgeDomain.Models
+{
+    public class User
+    {
+    }
+}
---
+++ TestingStudentKnowledge/TestingStudentKnowledgeInfrastructureData/Data/TestingStudentKnowledgeDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestingStudentKnowledgeDomain.Models;
+
+namespace TestingStudentKnowledgeInfrastructureData.Data
+{
+    public class TestingStudentKnowledgeDbContext
+    {
+        public TestingStudentKnowledgeDbContext()
+        {
+
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    // Should in future implement optionsBuilder.UseSqlServer ... something like this
+        //}
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Guest> Guests { get; set; }
+        public DbSet<User> Users { get; set; }
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Admin>().ToTable("Admins");
+        //    modelBuilder.Entity<Guest>().ToTable("Guests");
+        //    modelBuilder.Entity<User>().ToTable("Users");
+        //}
+    }
+}
